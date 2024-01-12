81bf69c9e4987dc4f91dedca220a04943054224d
EmmeKoZ <emme.ko.zzz@gmail.com>
2024-01-12 09:49:18 -0500

    create login arquitecture

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import AuthPage from "./pages/login/auth_page.jsx";
+
 function App() {
   return (
-    <div className="h-full w-full flex items-center justify-center">
-      <h1 className="text-3xl">Start Point!</h1>
-    </div>
+    <>
+      <AuthPage />
+    </>
   );
 }
 
---
+++ src/pages/login/auth_page.jsx
@@ -0,0 +1,10 @@
+function AuthPage() {
+  return (
+    // centered box
+    <div className="w-full h-full flex items-center justify-center">
+      {/* login panel */}
+    </div>
+  );
+}
+
+export default AuthPage;
