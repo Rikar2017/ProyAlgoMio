1954862f68ab79227872c811ce2ff55dabef5bde
EmmeKoZ <emme.ko.zzz@gmail.com>
2024-01-12 16:10:42 -0500

    setup daisyui

--- src/pages/login/auth_page.jsx
+++ src/pages/login/auth_page.jsx
@@ -1,8 +1,19 @@
+import { useState } from "react";
+
 function AuthPage() {
+  const [theme, setTheme] = useState(true);
+
   return (
     // centered box
     <div className="w-full h-full flex items-center justify-center">
       {/* login panel */}
+      <button
+        data-theme={theme ? "light" : "dark"}
+        className="btn btn-primary"
+        onClick={() => setTheme(!theme)}
+      >
+        Start!
+      </button>
     </div>
   );
 }
